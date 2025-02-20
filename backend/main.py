87b87463856b0eb16fa75b0c68dcc6848a6dc012
Aditya <71001844+zendrix396@users.noreply.github.com>
2025-02-20 18:42:40 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -19,7 +19,7 @@ UPLOAD_DIR.mkdir(exist_ok=True)
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "https://health-vault-1.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
